38e23977f01c3c62042cfac69c616ee6c98cf9d1
Igaguri <igagurimk@gmail.com>
2018-06-16 15:54:06 +0900

    rot と shift を追加

--- transform.go
+++ transform.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math"
 	"strconv"
 	"sync"
 	"time"
@@ -37,6 +38,10 @@ func main() {
 		":add":    add{},
 		":after":  after{},
 		":before": before{},
+		":rot-x":  rot{"x"},
+		":rot-y":  rot{"y"},
+		":rot-z":  rot{"z"},
+		":shift":  shift{},
 	}
 
 	args := newArgList()
@@ -144,6 +149,10 @@ func (cc cut) Filter(ch <-chan sysCoord.Coord, args *argList) <-chan sysCoord.Co
 	return filtered
 }
 
+func getX(c sysCoord.Coord) float32 { return c.X }
+func getY(c sysCoord.Coord) float32 { return c.Y }
+func getZ(c sysCoord.Coord) float32 { return c.Z }
+
 type add struct{}
 
 func (a add) Filter(ch <-chan sysCoord.Coord, args *argList) <-chan sysCoord.Coord {
@@ -173,14 +182,11 @@ func (a after) Filter(ch <-chan sysCoord.Coord, args *argList) <-chan sysCoord.C
 	filtered := make(chan sysCoord.Coord, sysCoord.StreamBufferSize)
 
 	go func() {
-		cnt := 0
 		for c := range ch {
 			if c.Date >= thres {
 				filtered <- c
-				cnt++
 			}
 		}
-		log.Println("Hit:", cnt)
 		close(filtered)
 	}()
 
@@ -195,20 +201,94 @@ func (b before) Filter(ch <-chan sysCoord.Coord, args *argList) <-chan sysCoord.
 	filtered := make(chan sysCoord.Coord, sysCoord.StreamBufferSize)
 
 	go func() {
-		cnt := 0
 		for c := range ch {
 			if c.Date < thres {
 				filtered <- c
-				cnt++
 			}
 		}
-		log.Println("Hit:", cnt)
 		close(filtered)
 	}()
 
 	return filtered
 }
 
-func getX(c sysCoord.Coord) float32 { return c.X }
-func getY(c sysCoord.Coord) float32 { return c.Y }
-func getZ(c sysCoord.Coord) float32 { return c.Z }
+type rot struct {
+	Axis string
+}
+
+func (r rot) Filter(ch <-chan sysCoord.Coord, args *argList) <-chan sysCoord.Coord {
+	deg := args.PopFloat32()
+	rad := math.Pi * float64(deg) / 180
+	c := float32(math.Cos(rad))
+	s := float32(math.Sin(rad))
+
+	var m [3][3]float32
+	switch r.Axis {
+	case "x":
+		m = [3][3]float32{
+			[3]float32{1, 0, 0},
+			[3]float32{0, c, -s},
+			[3]float32{0, s, c},
+		}
+	case "y":
+		m = [3][3]float32{
+			[3]float32{c, 0, s},
+			[3]float32{0, 1, 0},
+			[3]float32{-s, 0, c},
+		}
+	case "z":
+		m = [3][3]float32{
+			[3]float32{c, -s, 0},
+			[3]float32{s, c, 0},
+			[3]float32{0, 0, 1},
+		}
+	}
+
+	shifted := make(chan sysCoord.Coord, sysCoord.StreamBufferSize)
+
+	go func() {
+		cnt := 0
+		for c := range ch {
+			rc := sysCoord.Coord{
+				X:    m[0][0]*c.X + m[0][1]*c.Y + m[0][2]*c.Z,
+				Y:    m[1][0]*c.X + m[1][1]*c.Y + m[1][2]*c.Z,
+				Z:    m[2][0]*c.X + m[2][1]*c.Y + m[2][2]*c.Z,
+				Date: c.Date,
+			}
+			shifted <- rc
+		}
+		log.Println("Hit:", cnt)
+		close(shifted)
+	}()
+
+	return shifted
+}
+
+type shift struct {
+	Axis string
+}
+
+func (s shift) Filter(ch <-chan sysCoord.Coord, args *argList) <-chan sysCoord.Coord {
+	dx := args.PopFloat32()
+	dy := args.PopFloat32()
+	dz := args.PopFloat32()
+
+	shifted := make(chan sysCoord.Coord, sysCoord.StreamBufferSize)
+
+	go func() {
+		cnt := 0
+		for c := range ch {
+			rc := sysCoord.Coord{
+				X:    c.X + dx,
+				Y:    c.Y + dy,
+				Z:    c.Z + dz,
+				Date: c.Date,
+			}
+			shifted <- rc
+		}
+		log.Println("Hit:", cnt)
+		close(shifted)
+	}()
+
+	return shifted
+}
